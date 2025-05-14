52af6f394f35b6ab78d5d999967bdaebd5ef800a
saranyakp-88 <saranya.paramesh@gmail.com>
2025-05-14 17:38:18 +0530

    Add files via upload
    Week 1 Day 1 HW assignment

---
+++ day1hw/CheckNumberIsPositive.java
@@ -0,0 +1,16 @@
+package week1.day1hw;
+
+public class CheckNumberIsPositive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//positive and negative number HW
+		
+		int version = 10 ;
+		if (version > 0) {
+			System.out.println("The number is positive");
+		}
+		else System.out.println("the number is negative");
+	}
+
+}
---
+++ day1hw/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1hw;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Datatypes HW
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("The version is:"+version+"\n"+"The developer is:"+developer+"\n"+"Beta is:"+isBeta+"\n"+"Year released is:"+releaseYear+"\n"+"Shortcut key is:"+shortcutKey);
+		
+		
+	}
+
+}
